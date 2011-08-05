4678fc98ad3c702cec9d208d275a99612a31275c
Hong Jiang <hong.jiang@onycloud.com>
2011-08-05 13:28:28 +0800

    Switch to our own version of clojure-control.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
-(defproject lein-control "0.1.1"
+(defproject org.clojars.onycloud/lein-control "0.1.1-SNAPSHOT"
   :description "leiningen clojure-control plugin"
-  :author "Sun Ning <classicning@gmail.com>"
   :dependencies [[org.clojure/clojure "1.2.1"]
-                            [control "0.2.0"]]
+                 [org.clojars.onycloud/control "0.2.0-SNAPSHOT"]]
   :dev-dependencies [[lein-clojars "0.6.0"]])
